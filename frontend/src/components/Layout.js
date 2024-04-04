4eeba89c2ccff80f4c0cfc654cfa21872ede0f8f
Anuj_Maurya <am398@snu.edu.in>
2024-04-04 19:47:58 +0530

    Mobile Sidebar and Navbar Added

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,35 +1,35 @@
 import React from 'react';
 import { Outlet, useLocation, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import  Navbar  from './Navbar';
+import Navbar from './Navbar';
 import Sidebar from './Sidebar';
-// import { MobileSidebar } from './MobileSidebar';
+import MobileSidebar from './MobileSidebar';
 
 
 export const Layout = () => {
     // const { user } = useSelector((state) => state.auth);
 
     // const location = useLocation();
-  
+
     return (
-      <div className='w-full h-screen flex flex-col md:flex-row'>
-        <div className='w-1/5 h-screen bg-white sticky top-0 hidden md:block'>
-          <Sidebar />
+        <div className='w-full h-screen flex flex-col md:flex-row'>
+            <div className='w-1/5 h-screen bg-white sticky top-0 hidden md:block'>
+                <Sidebar />
+            </div>
+
+            <MobileSidebar />
+
+            <div className='flex-1 overflow-y-auto'>
+                <Navbar />
+
+                <div className='p-4 2xl:px-10'>
+                    <Outlet />
+                </div>
+            </div>
         </div>
-  
-        {/* <MobileSidebar /> */}
-  
-        <div className='flex-1 overflow-y-auto'>
-          <Navbar />
-  
-          <div className='p-4 2xl:px-10'>
-            <Outlet />
-          </div>
-        </div>
-      </div>
     );
 }
 
-    // : (
-    //   <Navigate to='/log-in' state={{ from: location }} replace />
-    // );
+// : (
+//   <Navigate to='/log-in' state={{ from: location }} replace />
+// );
