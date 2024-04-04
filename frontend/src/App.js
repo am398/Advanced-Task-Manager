99fec5784b93642769301983f19f8a82792ca6b9
Anuj_Maurya <am398@snu.edu.in>
2024-04-04 20:18:13 +0530

    Dashboard Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
-import { Dashboard } from "./pages/Dashboard";
+import Dashboard from "./pages/Dashboard";
 import { Layout } from "./components/Layout";
 import  Login  from "./pages/Login";
 import { Tasks } from "./pages/Tasks";
